765b2d3fbaab0f8b6ac55dd0a2370140704a9435
nikolaskyr <45922282+nikolaskyr@users.noreply.github.com>
2019-03-16 20:16:43 +0000

    Add files via upload

--- lets_watch_project/settings.py
+++ lets_watch_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'letswatch',
+    'search',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,8 @@ ROOT_URLCONF = 'lets_watch_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': [TEMPLATE_DIR,
+                 '/search/search.html', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- lets_watch_project/urls.py
+++ lets_watch_project/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^letswatch/', include('letswatch.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^search/', include('search.urls', namespace='search')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
