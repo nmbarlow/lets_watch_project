ebc61b6f6a22a0c5c562b0c14fc062330b40c54e
Nadya Barlow <nadyabarlow@hotmail.com>
2019-03-19 00:34:15 +0000

    movie population script, but year wont display

--- letswatch/admin.py
+++ letswatch/admin.py
@@ -4,7 +4,7 @@ from letswatch.models import Genre, Movie, UserProfile, VideoPost, Comment
 # Register your models here.
 # class to customise the Admin Interface
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'genre', 'url')
+    list_display = ('title', 'genre', 'url', 'year')
 
 class GenreAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('name',)}
--- populate_letswatch.py
+++ populate_letswatch.py
@@ -16,28 +16,116 @@ def populate():
 
     comedy_movies = [
         {"title" : "The Wolf of Wall Street",
-         "url" : "https://www.youtube.com/watch?v=iszwuX1AK6A"},
+         "url" : "https://www.youtube.com/watch?v=iszwuX1AK6A",
+         "year" : "2013"},
+        {"title" : "Bridesmaids",
+         "url" : "https://www.youtube.com/watch?v=FNppLrmdyug",
+         "year" : "2011"},
+        {"title" : "Mean Girls",
+         "url" : "https://www.youtube.com/watch?v=KAOmTMCtGkI",
+         "year" : "2004"},
+        {"title" : "Super Bad",
+         "url" : "https://www.youtube.com/watch?v=4eaZ_48ZYog",
+         "year" : "2007"}
     ]
 
     horror_movies =[
         {"title" : "Get Out",
-         "url" : "https://www.youtube.com/watch?v=DzfpyUB60YY"}
+         "url" : "https://www.youtube.com/watch?v=DzfpyUB60YY",
+         "year" : "2017"},
+        {"title" : "IT",
+         "url" : "https://www.youtube.com/watch?v=FnCdOQsX5kc",
+         "year" : "2017"}
+
+    ]
+    action_movies =[
+        {"title" : "Wonder Woman",
+         "url" : "https://www.youtube.com/watch?v=1Q8fG0TtVAY",
+         "year" : "2017"},
+        {"title" : "Mad Max: Fury Road",
+         "url" : "https://www.youtube.com/watch?v=hEJnMQG9ev8",
+         "year" : "2015"}
+
+    ]
+    romance_movies =[
+        {"title" : "The Notebook",
+         "url" : "https://www.youtube.com/watch?v=FC6biTjEyZw",
+         "year" : "2004"},
+        {"title" : "A Star is Born",
+         "url" : "https://www.youtube.com/watch?v=nSbzyEJ8X9E",
+         "year" : "2018"},
+        {"title" : "Love Actually",
+         "url" : "https://www.youtube.com/watch?v=fOS-HMiVejo",
+         "year" : "2003"}
+
+    ]
+    family_movies =[
+        {"title" : "Mary Poppins",
+         "url" : "https://www.youtube.com/watch?v=nOfH7uEojKk",
+         "year" : "1964"},
+        {"title" : "Mary Poppins Returns",
+         "url" : "https://www.youtube.com/watch?v=gZgUW88D15w",
+         "year" : "2018"},
+        {"title" : "Mrs Doubtfire",
+         "url" : "https://www.youtube.com/watch?v=i8bONcsjaVo",
+         "year" : "1993"}
+
+
+    ]
+    western_movies =[
+        {"title" : "Django Unchained",
+         "url" : "https://www.youtube.com/watch?v=_iH0UBYDI4g",
+         "year" : "2012"},
+        {"title" : "The Magnificent Seven",
+         "url" : "https://www.youtube.com/watch?v=q-RBA0xoaWU",
+         "year" : "2016"},
+        {"title" : "The Magnificent Seven",
+         "url" : "https://www.youtube.com/watch?v=bG-ZxrG7htU",
+         "year" : "1960"}
+
+    ]
+
+    scifi_movies =[
+        {"title" : "Interstellar",
+         "url" : "https://www.youtube.com/watch?v=zSWdZVtXT7E",
+         "year" : "2014"},
+        {"title" : "Star Wars: The Force Awakens",
+         "url" : "https://www.youtube.com/watch?v=sGbxmsDFVnE",
+         "year" : "2015"}
+
+    ]
+    animation_movies =[
+        {"title" : "Shrek 2",
+         "url" : "https://www.youtube.com/watch?v=V6X5ti4YlG8",
+         "year" : "2004"},
+        {"title" : "Incredibles 2",
+         "url" : "https://www.youtube.com/watch?v=i5qOzqD9Rms",
+         "year" : "2018"},
+        {"title" : "Frozen",
+         "url" : "https://www.youtube.com/watch?v=TbQm5doF_Uc",
+         "year" : "2013"}
 
     ]
 
     gens = {"comedy" : {"movies" : comedy_movies,},
-            "horror" : {"movies" : horror_movies,}}
+            "horror" : {"movies" : horror_movies,},
+            "action" : {"movies" : action_movies,},
+            "romance" : {"movies" : romance_movies,},
+            "family" : {"movies" : family_movies,},
+            "western" : {"movies" : western_movies,},
+            "scifi" : {"movies" : scifi_movies,},
+            "animation" : {"movies" : animation_movies,},}
 
     for gen, gen_data in gens.items():
         g = add_gen(gen) # Do genre first as movie needs genre reference
         for m in gen_data["movies"]:
-            add_movie(g, m["title"], m["url"],)
+            add_movie(g, m["title"], m["url"], m["year"])
 
     for g in Genre.objects.all():
         for m in Movie.objects.filter(genre=g):
             print( "- {0} - {1}". format(str(g), str(m)))
 
-def add_movie(gen, title, url,):
+def add_movie(gen, title, url, year):
     m = Movie.objects.get_or_create(genre=gen, title=title)[0]
     m.url=url
     m.save()
