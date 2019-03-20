47757af0e67fcf8818b90c76a1af82043019da03
nikolaskyr <45922282+nikolaskyr@users.noreply.github.com>
2019-03-20 01:41:36 +0000

    Add files via upload

--- lets_watch_project/settings.py
+++ lets_watch_project/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'lets_watch_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
