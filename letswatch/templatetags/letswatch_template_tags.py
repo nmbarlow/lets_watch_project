3a9a8de31fe3e57080a9c14dffa527a44b3e8ecf
Tala Moin <talamoin@gmail.com>
2019-03-21 04:38:05 +0000

    edited index + base

--- letswatch/templatetags/letswatch_template_tags.py
+++ letswatch/templatetags/letswatch_template_tags.py
@@ -1,5 +1,7 @@
 from django import template
+from django.template import Template, Context
 from letswatch.models import Genre,Review
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -7,6 +9,10 @@ register = template.Library()
 def get_genre_list():
     return {'gens':Genre.objects.all()}
 
+
+@register.inclusion_tag('letswatch/show_genresss.html')
+def genre_template():
+	return {'gens':Genre.objects.all()}
 
 
 def get_review_list():
