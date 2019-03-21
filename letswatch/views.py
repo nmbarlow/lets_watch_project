745c1e6e55c5e726c6680f249551dfdaeb73561f
nikolaskyr <45922282+nikolaskyr@users.noreply.github.com>
2019-03-21 21:30:06 +0000

    Add files via upload

--- letswatch/views.py
+++ letswatch/views.py
@@ -9,6 +9,10 @@ from django.contrib.auth import logout
 from letswatch.models import UserProfile
 from django.contrib.auth.models import User
 from django.db.models import Q
+from social_django.models import UserSocialAuth
+from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.http import JsonResponse
 import json
@@ -528,3 +532,47 @@ def profile(request, username):
             print(form.errors)
 
     return render(request, 'letswatch/profile_registeration.html',{'userprofile': userprofile, 'selecteduser': user, 'form': form})
+
+"""
+@login_required
+def settings(request):
+    user = request.user
+    
+            try:
+            facebook_login = user.social_auth.get(provider='facebook')
+        except UserSocialAuth.DoesNotExist:
+            facebook_login = None
+
+    try:
+        twitter_login = user.social_auth.get(provider='twitter')
+    except UserSocialAuth.DoesNotExist:
+        twitter_login = None
+
+    can_disconnect = (user.social_auth.count() > 1 or user.has_usable_password())
+
+    return render(request, 'letswatch/settings.html', {
+        'twitter_login': twitter_login,
+        # 'facebook_login': facebook_login,
+        'can_disconnect': can_disconnect
+    })
+
+@login_required
+def password(request):
+    if request.user.has_usable_password():
+        PasswordForm = PasswordChangeForm
+    else:
+        PasswordForm = AdminPasswordChangeForm
+
+    if request.method == 'POST':
+        form = PasswordForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordForm(request.user)
+    return render(request, 'core/password.html', {'form': form})
+"""
